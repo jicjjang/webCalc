4a4f85c528d4059ffa23906313ba2c621e801e6f
jicjjang <jicjjang12@gmail.com>
2017-11-19 03:05:57 +0900

    append article toggle animations

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,27 @@
 'use strict';
 
+(function () {
+  var body = document.querySelector('body');
+  var minifyCalcButton = document.querySelectorAll('main > header > section > button')[1];
+  var articles = document.querySelectorAll('article');
+
+  minifyCalcButton.addEventListener('click', function () {
+    toggleArticle(articles[0], articles[1]);
+  });
+
+  articles[1].addEventListener('click', function () {
+    toggleArticle(articles[1], articles[0]);
+  });
+})();
+
+function toggleArticle(target1, target2) {
+  target1.style.opacity = 0;
+  setTimeout(function () {
+    target2.style = '';
+    setTimeout(function () {
+      target1.className = '';
+      target1.style.display = 'none';
+      target2.className = 'activate';
+    }, 800);
+  }, 800);
+}
